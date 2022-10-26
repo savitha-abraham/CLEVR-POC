8d73e7e230ff47fd568bf6f2b2080e5cefd259ee
marjan <marycore@gmail.com>
2022-10-26 13:55:11 +0200

    fixing test

--- neural-baselines/clip/engine.py
+++ neural-baselines/clip/engine.py
@@ -70,6 +70,7 @@ def train(final_classifier, clip_model, dataloader, optimizer, criterion, train_
         # update optimizer parameters
         optimizer.step()
 
+
         
     train_loss = train_running_loss / counter
     return train_loss
@@ -115,29 +116,50 @@ def validate(final_classifier, clip_model, dataloader, criterion, val_data, devi
 
 
 
+def test(final_classifier, clip_model, dataloader, criterion, test_data, device, dropout, clip_processor):
+    print('Testing')
+    final_classifier.eval()
+    counter = 0
+    correct = 0
+    test_running_loss = 0.0
+    for i, data in tqdm(enumerate(dataloader), total=int(len(test_data)/dataloader.batch_size)):
+        counter += 1
+        
+        data_device = to_device(data, device)
+        images = list(map(get_pil_image, data_device['image_path']))
+       
+        
+        inputs = clip_processor(text=data_device['question'], images=images, return_tensors="pt", padding=True)
+        inputs = inputs.to(device)
+        clip_output = clip_model(**inputs)
+            
+        text_emb = clip_output['text_embeds']
+        image_emb = clip_output['image_embeds']
+        constraint_type_embedding = data_device['constraint_embedding']
 
-def test(model, dataloader, device):
-    for counter, data in enumerate(dataloader):
-        image, target = data['image'].to(device), data['label']
-        # get all the index positions where value == 1
-        target_indices = [i for i in range(len(target[0])) if target[0][i] == 1]
-        # get the predictions by passing the image through the model
-        outputs = model(image)
-        outputs = torch.sigmoid(outputs)
-        outputs = outputs.detach().cpu()
-        sorted_indices = np.argsort(outputs[0])
-        best = sorted_indices[-3:]
-        string_predicted = ''
-        string_actual = ''
-        for i in range(len(best)):
-            string_predicted += f"{genres[best[i]]}    "
-        for i in range(len(target_indices)):
-            string_actual += f"{genres[target_indices[i]]}    "
-        image = image.squeeze(0)
-        image = image.detach().cpu().numpy()
-        image = np.transpose(image, (1, 2, 0))
-        plt.imshow(image)
-        plt.axis('off')
-        plt.title(f"PREDICTED: {string_predicted}\nACTUAL: {string_actual}")
-        plt.savefig(f"../outputs/inference_{counter}.jpg")
-        plt.show()
+        emb = torch.cat([text_emb, image_emb, constraint_type_embedding], dim=1)          
+        emb = dropout(emb)
+          
+        outputs = final_classifier(emb)
+
+        # apply sigmoid activation to get all the outputs between 0 and 1
+        outputs = torch.sigmoid(outputs)        
+        
+        loss = criterion(outputs, data_device['target'])
+        test_running_loss += loss.item()
+
+        pred = torch.max(outputs, 1)[1]
+        t = torch.max(data_device['target'], 1)[1]
+        correct += pred.eq(t).sum()
+
+        """
+        ## assign 0 label to those with less than 0.5
+        outputs[outputs >= 0.5] = 1
+        outputs[outputs < 0.5] = 0 
+
+        N,C = data_device['target'].shape
+        accuracy = (outputs == data_device['target']).sum() / (N*C) * 100   
+        """
+    test_loss = test_running_loss / counter
+    accuracy = 100 * correct / counter
+    return test_loss, accuracy
